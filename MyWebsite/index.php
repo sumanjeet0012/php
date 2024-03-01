eeef3b7779ed517a9b57fcd4a2e8bd93e571698e
Sumanjeet <sumanjeet0012@gmail.com>
2024-03-02 01:16:51 +0530

    basic syntax

---
+++ MyWebsite/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>hi</h1>
+    <p>hello</p>
+    <?php 
+    echo "Hello World!";
+    ?> -->
+
+    <!-- <?php 
+    if(true){
+        echo "<p>hello</p>";
+    }
+    ?> -->
+
+<?php if (true) { ?>
+<p>paragraph tag is used here</p>
+<?php } ?>
+
+
+</body>
+</html>
