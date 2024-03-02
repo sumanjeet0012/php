2aeb121890d69ef912e65c2f1b70fbc6af4b4327
Sumanjeet <sumanjeet0012@gmail.com>
2024-03-02 11:06:57 +0530

    variables and data types

--- MyWebsite/index.php
+++ MyWebsite/index.php
@@ -18,9 +18,53 @@
     }
     ?> -->
 
-<?php if (true) { ?>
+<!-- <?php if (true) { ?>
 <p>paragraph tag is used here</p>
-<?php } ?>
+<?php } ?> -->
+
+<!-- <p>This is a <?php echo " php embedded " ?> paragraph</p>
+<?php 
+    echo "This is a PHP paragraph";
+?> -->
+
+
+<!-- <?php 
+    $name = "sumanjeet";
+    echo $name;
+?> -->
+
+<?php 
+
+    // Scalar types (contains one value)
+    // $string = "Daniel";
+
+    // $int = 12354353;
+
+    // $float = 2.5678;
+
+    // $bool = true;
+
+    // // Array type
+    // $names = array("Daniel", "Bella", "Frida");
+    // $names_2 = ["Daniel", "Bella", "Frida"];
+
+    // Default values 
+
+
+    // $string = "";
+    // $int = 0;
+    // $float = 0;
+
+    // $bool = false;
+
+    // $array = [];
+    // $object = null;  
+
+    $name;
+    echo "Hello World!";
+    echo $name;
+?>
+
 
 
 </body>
