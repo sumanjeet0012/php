b9fd00cbc1fc4eeaa11b30555666e9d1d9cc2438
Sumanjeet <sumanjeet0012@gmail.com>
2024-03-04 02:27:30 +0530

    constants in php

---
+++ constants/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    define("PI", 3.14); // constant variable should be capatalized.
+    define("NAME", "Daniel");
+    define("IS_ADMIN", true);
+
+    function test()
+    {
+        echo PI;
+
+    }
+
+    test();
+
+    ?>
+</body>
+
+</html>
